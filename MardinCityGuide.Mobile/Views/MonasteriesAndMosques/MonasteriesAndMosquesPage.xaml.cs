b3f87a78f0f08fec30292d9cd52bbc8cbee3f685
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2026-08-16 22:04:10 +0300

    OnBackButtonClicked ve OnSearchButtonClicked metotları eklendi

--- MardinCityGuide.Mobile/Views/MonasteriesAndMosques/MonasteriesAndMosquesPage.xaml.cs
+++ MardinCityGuide.Mobile/Views/MonasteriesAndMosques/MonasteriesAndMosquesPage.xaml.cs
@@ -5,5 +5,15 @@ public partial class MonasteriesAndMosquesPage : ContentPage
 	public MonasteriesAndMosquesPage()
 	{
 		InitializeComponent();
+	}	
+
+	private void OnBackButtonClicked(object sender, EventArgs e)
+	{
+
+	}	
+
+	private void OnSearchButtonClicked(object sender, EventArgs e)
+	{
+
 	}
 }
